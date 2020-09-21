ebd3ec85eea6b99d048c1a5108ef770ae7dfdadc
Lan Pavletič <62104478+DolanStank@users.noreply.github.com>
2020-09-21 17:01:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,20 @@
 import random
+from copy import deepcopy
 
 
-test_board = [[0, 0, 2, 1, 7, 0, 0, 0, 6],
-              [0, 9, 0, 0, 0, 8, 0, 5, 3],
-              [0, 4, 0, 3, 0, 0, 0, 1, 8],
-              [0, 0, 0, 8, 0, 0, 6, 4, 0],
-              [9, 8, 0, 0, 2, 7, 0, 0, 1],
-              [0, 0, 3, 0, 9, 0, 0, 2, 7],
-              [5, 0, 1, 9, 0, 0, 0, 7, 0],
-              [0, 7, 0, 4, 5, 1, 9, 6, 0],
-              [4, 2, 9, 0, 3, 0, 0, 0, 0]]
-
-empty_board = [[0, 0, 0, 0, 0, 0, 0, 0, 0],
-               [0, 0, 0, 0, 0, 0, 0, 0, 0],
-               [0, 0, 0, 0, 0, 0, 0, 0, 0],
-               [0, 0, 0, 0, 0, 0, 0, 0, 0],
-               [0, 0, 0, 0, 0, 0, 0, 0, 0],
-               [0, 0, 0, 0, 0, 0, 0, 0, 0],
-               [0, 0, 0, 0, 0, 0, 0, 0, 0],
-               [0, 0, 0, 0, 0, 0, 0, 0, 0],
-               [0, 0, 0, 0, 0, 0, 0, 0, 0]]
-
-
-# ---NEEDS A FIX-------
 def rotate_board_180(board):
-    new_board = empty_board
+    board_cpy = deepcopy(board)
     for i in range(9):
         for j in range(9):
-            new_board[i][j] = board[9 - j - 1][9 - i - 1]
-    return new_board
+            board[i][j] = board_cpy[8 - i][8 - j]
 
 
 def filled(board):
-    count = 0
     for i in range(9):
         for j in range(9):
             if board[i][j] == 0:
-                count += 1
-    return count <= True
+                return False
+    return True
 
 
 def candidates(board, x, y):
@@ -57,9 +34,9 @@ def field_with_fewest_possible_candidates(board):
     for i in range(9):
         for j in range(9):
             if board[i][j] == 0:
-                num_of_canidates = len(candidates(board, j, i))
-                if num_of_canidates < count:
-                    count = num_of_canidates
+                num_of_candidates = len(candidates(board, j, i))
+                if num_of_candidates < count:
+                    count = num_of_candidates
                     x, y = j, i
     return x, y
 
@@ -109,6 +86,8 @@ def valid(board, x, y):
 
 
 def solve(board):
+    if not valid_board(board):
+        return False
     if not find_first_empty_field(board):
         return True
 
@@ -121,15 +100,6 @@ def solve(board):
                 return True
         board[y][x] = 0
     return False
-
-
-def solveable(board):
-    board_cpy = board[:]
-
-    if solve(board_cpy):
-        return True
-    else:
-        return False
 
 
 def generate_board(board):
@@ -148,37 +118,30 @@ def generate_board(board):
     board[y][x] = 0
     return False
 
+def solvable(board):
 
-def generate_puzzle(board):
+    board_cpy1 = deepcopy(board)
+    board_cpy2 = deepcopy(board)
+
+    solve(board_cpy1)
+    rotate_board_180(board_cpy2)
+    solve(board_cpy2)
+    rotate_board_180(board_cpy2)
+    return board_cpy2 == board_cpy1
+
+def generate_puzzle():
+    board = [[0 for x in range(9)] for y in range(9)]
     generate_board(board)
-
-    start_x, start_y = random.randint(0, 8), random.randint(0, 8)
-    board[start_y][start_x] = 0
-    print_board(board)
-    print(" ")
 
     i = 0
     while i < 50:
-        while True:
-            start_x_cpy = start_x
-            start_y_cpy = start_y
+        start_x, start_y = random.randint(0, 8), random.randint(0, 8)
+        if not (board[start_y][start_x] == 0):
+            value = board[start_y][start_x]
+            board[start_y][start_x] = 0
 
-            start_x += random.randrange(-1, 2)
-            start_y += random.randrange(-1, 2)
-
-            if not (0 <= start_x <= 8 and 0 <= start_y <= 8):
-                start_x = start_x_cpy
-                start_y = start_y_cpy
+            if solvable(board):
+                i += 1
             else:
-                break
-
-        cpy = board[start_y][start_x]
-        board[start_y][start_x] = 0
-        board_cpy = board
-        if solve(board) and solve(rotate_board_180(board)):
-            board = board_cpy
-            i += 1
-        else:
-            board[start_y][start_x] = cpy
-
-
+                board[start_y][start_x] = value
+    return board
